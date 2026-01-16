6d9a632dc1b8c01af45b1dc04103b5b341e6f808
Gifmaniac <w.vd.b@live.nl>
2026-01-16 12:06:34 +0100

    Liniting added.

--- bmthwebsite/bmth-frontend/bmth-frontend/eslint.config.js
+++ bmthwebsite/bmth-frontend/bmth-frontend/eslint.config.js
@@ -1,23 +1,33 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/cypress.config.ts",
+      "**/src/context/store/**",
+      "**/src/services/api/helper.ts",
+      "**/src/pages/Admin/Inventory/shared/useInventorySelection.ts",
+      "**/src/pages/Store/Shopdetail/ShirtDetail.tsx",
+      "**/src/pages/Store/cart/Cart.tsx",
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": "error",
     },
   },
-])
+];
